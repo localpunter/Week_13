659c1fc6b000a4068719cbfa1e4e1d1d763a221a
Alan Hunter <localpunter@sky.com>
2018-10-23 15:23:01 +0100

    Files and folders homework first commit

---
+++ day_02/FilesAndFolders/src/test/java/com/codeclan/example/FilesAndFolders/FilesAndFoldersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.FilesAndFolders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilesAndFoldersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
