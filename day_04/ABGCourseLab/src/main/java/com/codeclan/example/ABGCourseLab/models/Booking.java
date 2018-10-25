b5195b5df848038795f732499f0f72d15fc25564
Alan Hunter <localpunter@sky.com>
2018-10-25 11:12:38 +0100

    basic models built

--- day_04/ABGCourseLab/src/main/java/com/codeclan/example/ABGCourseLab/models/Booking.java
+++ day_04/ABGCourseLab/src/main/java/com/codeclan/example/ABGCourseLab/models/Booking.java
@@ -1,4 +1,63 @@
 package com.codeclan.example.ABGCourseLab.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String date;
+
+    @Column
+    private Course course;
+
+    @Column
+    private Customer customer;
+
+    public Booking() {
+    }
+
+    public Booking(Long id, String date, Course course, Customer customer) {
+        this.id = id;
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
